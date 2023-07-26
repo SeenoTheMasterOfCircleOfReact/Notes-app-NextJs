0e1228f718fa1c61582e689d5d8cf65f52d54776
seeno <sina.leagueoflegends@gmail.com>
2023-07-26 07:42:43 +0330

    changing some styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,5 @@
 import NotesList from '@/redux/features/notes/NotesList';
 
 export default function Home() {
-  return (
-    <div className="pt-24 grid grid-cols-1 lg:grid-cols-2 gap-4">
-      <NotesList />
-    </div>
-  );
+  return <NotesList />;
 }
--- src/redux/features/notes/NotesList.tsx
+++ src/redux/features/notes/NotesList.tsx
@@ -10,16 +10,16 @@ export default function NotesList() {
   // reordering the notes. new notes should be on top of the page
   const orderedNotes = [...notes].reverse();
   return (
-    <>
+    <div className="pt-24">
       {orderedNotes.length > 0 ? (
-        <>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
           {orderedNotes.map(note => (
             <Note key={note.id} note={note} />
           ))}
-        </>
+        </div>
       ) : (
         <div className="text-center pt-8">یادداشت های شما خالی است</div>
       )}
-    </>
+    </div>
   );
 }
